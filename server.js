77bfa581b4ed7e857ce29e91609b378441d7f7a5
Molly Moran <mollyclaremoran@gmail.com>
2018-04-09 11:46:58 -0500

    adds server file

---
+++ server.js
@@ -0,0 +1,41 @@
+// DEPENDENCIES
+// ==============================================================================
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const yelp = require('yelp-fusion');
+const client = yelp.client('a81GMhSX7f45Iq00D723hoEnNDYrwPRaxmkTpfbZ94ylgHc9lkH1BWjjhUSd93rZR8_xcjxLiyiOcnlHlVu3-SjghePZN60_1PXIJCPAQs_PcBxFXzsD8usRo0JzWnYx');
+
+// EXPRESS CONFIGURATION
+// ==============================================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("app/public"));
+
+// ROUTER
+// ================================================================================
+'use strict';
+
+app.get("/api/:latitude/:longitude", function(req, res) {
+
+    const latitude = req.params.latitude;
+    const longitude = req.params.longitude;
+    
+    client.search({
+        term:'restaurants',
+        price: 1,
+        radius: 3219,
+        latitude: latitude,
+        longitude: longitude
+    }).then(response => res.send(response.jsonBody.businesses[0]));
+});
+
+// LISTENER
+// =============================================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
